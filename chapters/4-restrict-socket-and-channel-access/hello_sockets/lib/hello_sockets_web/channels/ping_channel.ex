1b8419ad7df31cbd658ccaa0ca782c915752a72d
Exadra37 <exadra37@gmail.com>
2022-04-11 22:59:54 +0100

    Chapert 4 > Restrict Socket and Channel Access > Securing a Socket with Signed Tokens - Copied over the hello_sockets app from chapter 3.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ chapters/4-restrict-socket-and-channel-access/hello_sockets/lib/hello_sockets_web/channels/ping_channel.ex
@@ -0,0 +1,54 @@
+defmodule HelloSocketsWeb.PingChannel do
+
+  use HelloSocketsWeb, :channel
+
+  # without this handLe_out/3 will not intercept the `request_ping` broadcast.
+  intercept ["request_ping"]
+
+  @impl true
+  def join(_topic, _payload, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_in("ping:" <> phrase, _payload, socket) do
+    {:reply, {:ok, %{ping: phrase}}, socket}
+  end
+
+  @impl true
+  def handle_in("ping", %{"ack_phrase" => ack_phrase}, socket) do
+    {:reply, {:ok, %{ping: ack_phrase}}, socket}
+  end
+
+  @impl true
+  def handle_in("ping", _payload, socket) do
+    {:reply, {:ok, %{ping: "pong"}}, socket}
+  end
+
+  @impl true
+  def handle_in("param_ping", %{"error" => true}, socket) do
+    {:reply, {:error, %{reaon: "You asked for this!"}}, socket}
+  end
+
+  @impl true
+  def handle_in("param_ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  @impl true
+  def handle_in("pong", _payload, socket) do
+    # We only handle ping
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_in("ding", _payload, socket) do
+    {:stop, :shutdown, {:ok, %{msg: "shutting down"}}, socket}
+  end
+
+  @impl true
+  def handle_out("request_ping", payload, socket) do
+    push(socket, "send_ping", Map.put(payload, "from_node", Node.self()))
+    {:noreply, socket}
+  end
+end
